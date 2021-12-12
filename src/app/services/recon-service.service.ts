f75cb52ca9a026f3a5f25b54ac7d2b05700601de
Kevblum7 <88133417+Kevblum7@users.noreply.github.com>
2021-12-11 20:04:25 -0500

    Change get format

--- src/app/services/recon-service.service.ts
+++ src/app/services/recon-service.service.ts
@@ -1,4 +1,4 @@
-import { MissedAppointments, OverdueRecords, Alerts, VisitingList, PayerBased, RevenueAging, ReconSheet } from './../SheetTypeInterface';
+import { MissedAppointments, OverdueRecords, Alerts, VisitingList, PayerBased, RevenueAging, ReconSheet, PayerResponse } from './../SheetTypeInterface';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { NewPatient, } from '../SheetTypeInterface'
@@ -12,7 +12,7 @@ export class ReportsService{
   private reconURL = 'http://localhost:5000/reconciliationSheetBeanList'
   private newpatientURL = 'http://localhost:5000/newPatientsList'
   private missedAppURL = 'http://localhost:5000/missedAppointmentsList'
-  private payerURL = 'http://localhost:5000/payerBasedReportList'
+  private payerURL = 'https://payer-based-report-service.herokuapp.com/practice-management-reports/payer-based-report/date'
   private overdueURL = 'http://localhost:5000/overdueRecordsList'
   private visitingURL = 'http://localhost:5000/patientVisitingList'
   private alertsURL = 'http://localhost:5000/clinicalAlertsList'
@@ -46,8 +46,13 @@ export class ReportsService{
     
   }
 
-  getPayerBased(): Observable<PayerBased[]> {
-    return this.http.get<PayerBased[]>(this.payerURL)
+  getPayerBased(): Observable<Array<PayerBased>> {
+    return this.http.get<PayerResponse>(this.payerURL).pipe(
+      map( x => x.payerArray), 
+      catchError(error => {
+        throw new Error(error)
+      }))
+      
   }
 
   getRevenueAging(): Observable<RevenueAging[]> {
