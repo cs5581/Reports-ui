19e766e0d3812b562535ba33290aea267de29d97
Kevblum7 <88133417+Kevblum7@users.noreply.github.com>
2021-12-13 23:45:57 -0500

    Changed URLS and X inferred type

--- src/app/services/recon-service.service.ts
+++ src/app/services/recon-service.service.ts
@@ -13,21 +13,21 @@ import { catchError } from 'rxjs';
 export class ReportsService{
 
   private reconURL = 'https://reconciliation-sheet-service.herokuapp.com/practice-management-reports/reconciliation-sheet/date'
-  private newpatientURL = 'http://localhost:5000/newPatientsList'
-  private missedAppURL = 'http://localhost:5000/missedAppointmentsList'
-  private payerURL =   'http://localhost:5000/payerBasedReportList'// 'https://payer-based-report-service.herokuapp.com/practice-management-reports/payer-based-report/date'
-  private overdueURL = 'http://localhost:5000/overdueRecordsList'
-  private visitingURL = 'http://localhost:5000/patientVisitingList'
-  private alertsURL = 'http://localhost:5000/clinicalAlertsList'
+  private newpatientURL = 'https://new-patient-list-service.herokuapp.com/practice-management-reports/new-patient-list/date'  // 'http://localhost:5000/newPatientsList'
+  private missedAppURL = 'https://missed-appointments-service.herokuapp.com/practice-management-reports/missed-appointments/date' // 'http://localhost:5000/missedAppointmentsList'
+  private payerURL = 'https://payer-based-report-service.herokuapp.com/practice-management-reports/payer-based-report/date' // 'http://localhost:5000/payerBasedReportList'
+  private overdueURL = 'https://overdue-records-service.herokuapp.com/practice-management-reports/overdue-records' // 'http://localhost:5000/overdueRecordsList'
+  private visitingURL = 'https://patient-visiting-list-service.herokuapp.com/practice-management-reports/patient-visiting-list/date' // 'http://localhost:5000/patientVisitingList'
+  private alertsURL = 'https://clinical-alerts-service.herokuapp.com/practice-management-reports/clinical-alerts/date//' //'http://localhost:5000/clinicalAlertsList'
   private revenueURL = 'https://revenue-aging-report-service.herokuapp.com/practice-management-reports/revenue-aging-report/date/date'
   
 
   
   constructor(private http:HttpClient) { }
-
+  
   getRecon(): Observable<ReconSheet[]> {
     return this.http.get<ReconResponse>(this.reconURL).pipe(
-      map( x => x.reconciliationSheetBeanList), 
+      map( (x: { reconciliationSheetBeanList: any; }) => x.reconciliationSheetBeanList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -36,7 +36,7 @@ export class ReportsService{
 
   getNewPatient(): Observable<NewPatient[]> {
     return this.http.get<NewPatientResponse>(this.newpatientURL).pipe(
-      map( x => x.newPatientsList), 
+      map( (x: { newPatientsList: any; }) => x.newPatientsList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -44,7 +44,7 @@ export class ReportsService{
 
   getMissedApps(): Observable<MissedAppointments[]> {
     return this.http.get<MissedAppResponse>(this.missedAppURL).pipe(
-      map( x => x.missedAppointmentsList), 
+      map( (x: { missedAppointmentsList: any; }) => x.missedAppointmentsList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -52,7 +52,7 @@ export class ReportsService{
 
   getOverdueRecords(): Observable<OverdueRecords[]> {
     return this.http.get<OverdueResponse>(this.overdueURL).pipe(
-      map( x => x.overdueRecordsList), 
+      map( (x: { overdueRecordsList: any; }) => x.overdueRecordsList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -60,7 +60,7 @@ export class ReportsService{
 
   getAlerts(): Observable<Alerts[]> {
     return this.http.get<AlertsResponse>(this.alertsURL).pipe(
-      map( x => x.clinicalAlertsList), 
+      map( (x: { clinicalAlertsList: any; }) => x.clinicalAlertsList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -68,7 +68,7 @@ export class ReportsService{
 
   getVisitingList(): Observable<VisitingList[]> {
     return this.http.get<VisitingResponse>(this.visitingURL).pipe(
-      map( x => x.patientVisitingList), 
+      map( (x: { patientVisitingList: any; }) => x.patientVisitingList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -77,7 +77,7 @@ export class ReportsService{
 
   getPayerBased(): Observable<PayerBased[]> {
     return this.http.get<PayerResponse>(this.payerURL).pipe(
-      map( x => x.payerBasedReportList), 
+      map( (x: { payerBasedReportList: any; }) => x.payerBasedReportList), 
       catchError(error => {
         throw new Error(error)
       }))
@@ -87,7 +87,7 @@ export class ReportsService{
 
   getRevenueAging(): Observable<RevenueAging[]> {
     return this.http.get<RevenueResponse>(this.revenueURL).pipe(
-      map( x => x.revenueAgingReportList), 
+      map( (x: { revenueAgingReportList: any; }) => x.revenueAgingReportList), 
       catchError(error => {
         throw new Error(error)
       }))
